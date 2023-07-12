dacf5f71d3870fa5f81f94b4d2f625eafce7d1db
Kate <120708446+kateiscoding@users.noreply.github.com>
2023-07-13 00:36:00 +0900

    feat: added blog view & article page

---
+++ src/main/java/me/kate/springbootdeveloper/controller/BlogViewController.java
@@ -0,0 +1,37 @@
+package me.kate.springbootdeveloper.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.kate.springbootdeveloper.domain.Article;
+import me.kate.springbootdeveloper.dto.ArticleListViewResponse;
+import me.kate.springbootdeveloper.service.BlogService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class BlogViewController {
+
+    private final BlogService blogService;
+
+    @GetMapping("/articles")
+    public String getArticles(Model model) {
+        List<ArticleListViewResponse> articles = blogService.findAll().stream()
+                .map(ArticleListViewResponse::new)
+                .toList();
+        model.addAttribute("articles", articles);
+
+        return "articleList"; // articleList.html이라는 뷰
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleListViewResponse(article));
+
+        return "article";
+    }
+}
---
+++ src/main/java/me/kate/springbootdeveloper/dto/ArticleViewResponse.java
@@ -0,0 +1,24 @@
+package me.kate.springbootdeveloper.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.kate.springbootdeveloper.domain.Article;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+public class ArticleViewResponse {
+
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime createAt;
+
+    public ArticleViewResponse(Article article) {
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+        this.createAt = article.getCreatedAt();
+    }
+}
